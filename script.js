ac565e3ca32eb9b556af8a4409f9ea52700423b3
BryanGK <bryangordonkrauss@gmail.com>
2021-02-22 08:13:29 -0800

    Ongoing grid work

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 const canvas = document.querySelector('#game-board');
 const ctx = canvas.getContext('2d');
 
-const framesPerSecond = 60;
-const snakeSpeed = 1;
+const framesPerSecond = 10;
+const gridSize = 20;
+const snakeSpeed = 10;
 const snakeElementSize = 10;
 const snakeInitLength = 2;
-let snakeBodyLength = 2;
-let snakePosX = 300;
-let snakePosY = 250;
+let snakeBodyLength = 5;
+let snakePosX = 295;
+let snakePosY = 245;
 let directionX = 0;
 let directionY = 0;
+let applePosX = 0;
+let applePosY = 0;
 
 const drawGameBoard = () => {
-    ctx.fillStyle = 'green';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    for (let i = 0; i < canvas.height / gridSize; i++) {
+        for (let j = 0; j < canvas.width / gridSize; j++) {
+            ctx.fillStyle = 'green';
+            ctx.fillRect(gridSize * j, gridSize * i, gridSize, gridSize);
+        }
+    }
 }
 
 const drawHead = () => {
-    ctx.fillStyle = 'red';
-
+    ctx.fillStyle = 'yellow';
     getDirection();
-
-    boundaryCheck();
-
     ctx.fillRect(snakePosX += (snakeSpeed * directionX),
         snakePosY += (snakeSpeed * directionY),
         snakeElementSize,
@@ -30,12 +34,25 @@ const drawHead = () => {
 }
 
 const drawBody = () => {
-    ctx.beginPath();
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = snakeElementSize;
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, 0);
-    ctx.stroke();
+    ctx.fillStyle = 'black';
+    ctx.fillRect(snakePosX - (snakeElementSize * directionX),
+        (snakePosY - (snakeElementSize * directionY)),
+        snakeElementSize,
+        snakeElementSize);
+
+}
+
+const drawApple = () => {
+    ctx.fillStyle = 'red'
+    ctx.fillRect(applePosX, applePosY, snakeElementSize, snakeElementSize)
+}
+
+const randomX = () => {
+    return applePosX = Math.floor(Math.random() * (canvas.width - snakeElementSize)) + 1;
+}
+
+const randomY = () => {
+    return applePosY = Math.floor(Math.random() * (canvas.height - snakeElementSize)) + 1;
 }
 
 function getDirection() {
@@ -43,18 +60,18 @@ function getDirection() {
         switch (e.code) {
             case 'ArrowUp':
                 directionX = 0;
-                directionY = -snakeSpeed;
+                directionY = -1;
                 break;
             case 'ArrowDown':
                 directionX = 0;
-                directionY = snakeSpeed;
+                directionY = 1;
                 break;
             case 'ArrowLeft':
-                directionX = -snakeSpeed;
+                directionX = -1;
                 directionY = 0;
                 break;
             case 'ArrowRight':
-                directionX = snakeSpeed;
+                directionX = 1;
                 directionY = 0;
                 break;
             default:
@@ -64,7 +81,10 @@ function getDirection() {
     });
 }
 
-function boundaryCheck() {
+const appleCheck = () => {
+}
+
+const boundaryCheck = () => {
     if (snakePosX <= 0 || snakePosX >= canvas.width - snakeElementSize) {
         directionX = -directionX;
     }
@@ -78,5 +98,10 @@ window.onload = function () {
         drawGameBoard();
         drawBody();
         drawHead();
+        drawApple();
+        boundaryCheck();
+        appleCheck();
     }, 1000 / framesPerSecond);
+    randomX();
+    randomY();
 }
